85ca52333fc173cae374d30f5e6c3c39b93cddc7
krazyhel <patron@dyn-72-33-248-131.uwnet.wisc.edu>
2015-11-05 11:24:09 -0600

    Added Course table.

--- firstdjango/inventory/admin.py
+++ firstdjango/inventory/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 
 from .models import Item
 from .models import StudySessions
+from .models import Course
 
 class ItemAdmin(admin.ModelAdmin):
 	list_display = ['title', 'dueDate']
 	
 class StudySessionsAdmin(admin.ModelAdmin):
 	list_display = ['date', 'timeSpent']
+	
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ['course']
 
 admin.site.register(Item, ItemAdmin)
 admin.site.register(StudySessions, StudySessionsAdmin)
+admin.site.register(Course, CourseAdmin)
--- firstdjango/inventory/views.py
+++ firstdjango/inventory/views.py
@@ -3,13 +3,16 @@ from django.http import Http404
 
 from inventory.models import Item
 from inventory.models import StudySessions
+from inventory.models import Course
 
 def index(request):
 	items = Item.objects.all()
 	studySessions = StudySessions.objects.all()
+	course = Course.objects.all()
 	return render(request, 'inventory/index.html', {
 		'items': items,
 		'studySessions': studySessions,
+		'course': course
 	})
 
 def item_detail(request, id):
@@ -28,4 +31,13 @@ def studySession_detail(request, id):
 		raise Http404('This study sessions does not exist')
 	return render(request, 'inventory/studySession_detail.html', {
 		'studySession': studySession,
+	})
+	
+def course_detail(request, id):
+	try:
+		course = Course.objects.get(id=id)
+	except Course.DoesNotExist:
+		raise Http404('This course does not exist')
+	return render(request, 'inventory/course_detail.html', {
+		'course': course,
 	})
